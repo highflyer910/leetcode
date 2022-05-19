b970430f5674de08848602bf54e247772573b031
Thea <thea.mushambadze@gmail.com>
2022-05-20 01:16:52 +0400

    single number

--- single_number.py
+++ single_number.py
@@ -1,3 +1,19 @@
 # https://leetcode.com/problems/single-number/
 # Given an array of integers, every element appears twice except for one. Find that single one.
 
+# Any number XOR with itself is 0.
+# So, we can use XOR to find the single number.
+# Time - 0(n)
+# Space - 0(1)
+
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = 0
+        for num in nums:
+            result ^= num
+        return result
